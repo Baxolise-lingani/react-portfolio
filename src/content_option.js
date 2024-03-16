f30db76f6dfb394ee003adb4db2d86efc1e4bd17
Baxolise-lingani <princelingani@gmail.com>
2024-03-16 17:18:09 +0200

    No Changes

--- src/content_option.js
+++ src/content_option.js
@@ -1,155 +1,163 @@
-const logotext = "JOHN";
+const logotext = "Baxolise";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Baxolise Lingani",
+    description: "I’m Baxolise Lingani data Analyst, Software Full stack devloper,currently working at Younglings Africa (ABSA)",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Baxolise Lingani",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I'm a Software developer",
+        second: "Full Stack Developer",
+        third: "Data Analyst",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Crafting Code, Shaping Tomorrow: Empowering the Future, One Line at a Time.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "I'm a tech enthusiast who kickstarted my career as a Software Developer Intern at UVU Africa (CapaCiTi). There, I immersed myself in various projects, honing my skills and gaining hands-on experience in software development. Eager to broaden my expertise, I transitioned into the realm of data analysis as an intern at CPUT Library. This role allowed me to delve into the intricacies of data, extracting valuable insights and leveraging advanced analytical tools to drive informed decision-making.  Now, as a Junior Software Developer at Younglings Africa (ABSA), I continue to thrive in a dynamic environment that fosters growth and innovation. With a solid foundation in software development and a knack for data analysis, I bring a diverse skill set to the table. I am dedicated to crafting robust solutions and pushing the boundaries of technological innovation.    Throughout my journey, I have demonstrated a relentless drive to succeed, coupled with a passion for learning and growth. With a blend of practical experience, academic knowledge, and a hunger for challenges, I am well-equipped to make a meaningful impact in any project or team I'm a part of. Let's connect and explore opportunities to innovate together!",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Junior Software Developer",
+        where: "Younglings Africa(ABSA)",
+        date: "2024 - present",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Software Developer Intern",
+        where: "UVU Africa (CapaCiTi)",
+        date: "07/2023-01/2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Data Analyst Intern",
+        where: "Cape Peninsula University of Technology Library",
+        date: "06/2021 - 12/2021",
     },
 ];
 
 const skills = [{
         name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
         value: 80,
     },
     {
-        name: "React",
-        value: 60,
+        name: "HTML",
+        value: 90,
     },
     {
-        name: "Jquery",
+        name: "Javascript",
+        value: 75,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "CSS",
         value: 85,
+    },
+    {
+        name: "R",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 70,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-];
+// const services = [{
+//         title: "Figma Design",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+//     {
+//         title: "Web Development",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+//     {
+//         title: "Software Applicatins (Apps)",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+//     },
+// ];
 
 const dataportfolio = [{
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Bakery Website",
+        link: "https://stupendous-peony-023e04.netlify.app",
     },
     {
         img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Personal Website",
+        link: "https://cozy-khapse-b3af1e.netlify.app",
     },
     {
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Financial Calculator.",
+        link: "https://github.com/Baxolise-lingani/Financial-Calculator",
     },
     {
         img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "C Bank",
+        link: "https://github.com/Baxolise-lingani/bank-app",
     },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "princelingani@gmail.com",
+    YOUR_FONE: "0721065418",
+    description: "Let's work together!! ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_nx34gvq",
+    YOUR_TEMPLATE_ID: "template_ngap17n",
+    YOUR_USER_ID: "-PYCbJJd8pm4T5ZIp",
 };
 
 const socialprofils = {
-    github: "https://github.com",
+    github: "https://github.com/Baxolise-lingani",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/baxolise-lingani/",
     twitter: "https://twitter.com",
 };
 export {
@@ -158,7 +166,7 @@ export {
     dataportfolio,
     worktimeline,
     skills,
-    services,
+    // services,
     introdata,
     contactConfig,
     socialprofils,
